20d8b6666fdd6666dde44b5d578b5089177b2161
Shoaib Rizwan <104502052+imshoaibrizwan@users.noreply.github.com>
2024-05-24 15:41:42 +0500

    expense account updated

--- app/Models/Accounts/TransactionAccount.php
+++ app/Models/Accounts/TransactionAccount.php
@@ -80,13 +80,16 @@ class TransactionAccount extends Model
 
     public static function visaExpense_list()
     {
-        $list = self::where('PID', 18)->pluck('Trans_Acc_Name', 'id');
+        $subHead = SubHeadAccount::select('id')->whereIn('HID', [9, 10, 11])->get()->toArray();
+
+        $list = self::whereIn('PID', $subHead)->pluck('Trans_Acc_Name', 'id');
         return $list->prepend('Select', 0);
 
     }
 
     public static function visaExpense_dropdown($id = 0)
     {
+
         $res = self::visaExpense_list();
         $list = '';
         if ($res) {
