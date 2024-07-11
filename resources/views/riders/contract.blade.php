139fae4f1156a61a02fc6dcad9f94f157fdfd2c1
Shoaib Rizwan <104502052+imshoaibrizwan@users.noreply.github.com>
2024-07-11 16:52:53 +0500

    .

--- resources/views/riders/contract.blade.php
+++ resources/views/riders/contract.blade.php
@@ -200,9 +200,12 @@
             </td>
          </tr>
          <tr style="height:17pt">
-            <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#FFC000" colspan="4">
+            <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#FFC000" colspan="2">
                <p class="s1" style="padding-top: 2pt;padding-left: 24pt;text-indent: 0pt;text-align: left;">Delivery KM Range</p>
             </td>
+            <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt;text-align:center;" bgcolor="#FFC000" colspan="2">
+                <p class="s1" style="padding-top: 2pt;padding-left: 24pt;text-indent: 0pt;text-align: center;">Description</p>
+             </td>
             <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#FFC000" colspan="2">
                <p class="s1" style="padding-top: 2pt;text-indent: 0pt;text-align: center;">Rate</p>
             </td>
@@ -219,9 +222,12 @@
 
          @foreach($rider->items as $item)
          <tr style="height:15pt">
-            <td style="width:131pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" colspan="4">
+            <td style="width:131pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" colspan="2">
                <p class="s8" style="padding-top: 1pt;padding-left: 18pt;text-indent: 0pt;text-align: left;">{{@$item->item->item_name}}</p>
             </td>
+            <td style="width:131pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" colspan="2">
+                <p class="s8" style="padding-top: 1pt;padding-left: 18pt;text-indent: 0pt;text-align: left;">{{@$item->item->descriptions}}</p>
+             </td>
             <td style="width:85pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" colspan="2">
                <p class="s2" style="padding-top: 1pt;text-indent: 0pt;text-align: center;">{{@$item->price}}</p>
             </td>
